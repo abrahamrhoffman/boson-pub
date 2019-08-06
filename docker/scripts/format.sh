ab907f2f8538fcc01473c48ce92eea850989bf36
abhoffman <abhoffman@paypal.com>
2019-08-06 12:24:35 -0700

    v1.0.4 : scripts testing

--- docker/scripts/format.sh
+++ docker/scripts/format.sh
@@ -9,6 +9,15 @@ function verify_root () {
   fi
 }
 
+function verify_drive () {
+  local DRIVE=$1
+  if [[ "$DRIVE" == "" ]]
+  then
+    echo "Please provide a drive. Example: /dev/sdn"
+      exit 1
+  fi
+}
+
 function wipe () {
   echo -n "Wiping $1 ... "
   dd if=/dev/urandom of="$1" bs=1M count=1 &> /dev/null
@@ -21,6 +30,7 @@ function format () {
 
 function main () {
   verify_root
+  verify_drive
   wipe $1
 }
 
