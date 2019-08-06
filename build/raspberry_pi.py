f1d5de831a05f4278aa83013d7e401576d5628ed
abhoffman <abhoffman@paypal.com>
2019-08-06 15:19:27 -0700

    v1.0.5 : raspberry_pi build testing

--- build/raspberry_pi.py
+++ build/raspberry_pi.py
@@ -4,17 +4,22 @@ import glob
 import sys
 import os
 
+import bootstrap
+
 
 class RaspberryPi(object):
 
-    def __init__(self, local_drive, arch, verbose=False):
+    def __init__(self, local_drive, arch, container, verbose=False):
         self.devnull = open(os.devnull, "w")
-        self.verbose = verbose
+        self.container = container
         self.drive = local_drive
+        self.verbose = verbose
         self.arch = arch
 
-    def bootstrap(self):
-        pass
+    def start_bootstrap(self):
+        name = ("abehoffman/boson-pub:{}".format(self.container))
+        btstrp = bootstrap.BootStrap(self.container)
+        btstrp.run()
 
     def format(self):
         cmd = ("bash `pwd`/../docker/scripts/raspberry_pi/emmc/format.sh ")
@@ -27,18 +32,22 @@ class RaspberryPi(object):
         subprocess.call(cmd, shell=True)
 
     def run(self):
-        self.format()
-        self.get_binaries()
+        self.start_bootstrap()
+        #self.format()
+        #self.get_binaries()
 
 
 def main():
     parser = argparse.ArgumentParser()
     required = parser.add_argument_group('Required arguments')
+    required.add_argument("-i", "--image", action="store",
+                          help="Boson container version.  Ex: 1.0.5",
+                          required=True)
     required.add_argument("-d", "--drive", action="store",
                           help="Local drive to format and prepare. " + \
                                "Example: /dev/sdn", required=True)
     required.add_argument("-a", "--arch", action="store",
-                          help="CPU architecture. Example: 64, 32",
+                          help="CPU architecture: 64 [or] 32",
                           required=True)
     required.add_argument("-v", "--verbose", action="store_true",
                           help="Display output (default False)")
