9fd64dec05e6f6c14a9c3e929f004ff698913c97
abhoffman <abhoffman@paypal.com>
2019-08-06 13:52:45 -0700

    v1.0.4 : scripts testing

--- docker/scripts/format.sh
+++ docker/scripts/format.sh
@@ -19,26 +19,39 @@ function verify_drive () {
   local DRIVE=$1
   if [[ "$DRIVE" == "" ]]
   then
-    echo "Please provide a drive. Example: '/dev/sdn'"
+    echo "Must provide a drive. Example: '/dev/sdn'"
       exit 1
   fi
 }
 
 function wipe () {
-  echo -n "Wiping $1 ... "
-  dd if=/dev/urandom of="$1" bs=1M count=1 &> /dev/null
+  local DRIVE=$1
+  echo -n "Wiping $DRIVE : "
+  dd if=/dev/urandom of="$DRIVE" bs=1M count=1 &> /dev/null
   echo "Done"
 }
 
-function format () {
-  sudo parted 
+function create () {
+  local DRIVE=$1
+  local SIZE=`fdisk -l $DRIVE | awk '/sectors/' | grep "Disk" | awk '{print $3, $4}' | sed 's/\,//g'`
+
+  echo -n "Setting $DRIVE label 'msdos' : "
+  parted -s "${DRIVE}" mklabel msdos
+  echo "Done"
+
+  echo -n "Creating partition ${DRIVE}1 : "
+  parted -s $DRIVE -a optimal primary 0% ${SIZE}
+  echo "Done"
+
 }
 
 function main () {
+  local DRIVE=$1
   user_feedback
   verify_root
-  verify_drive $1
-  wipe $1
+  verify_drive $DRIVE
+  wipe $DRIVE
+  create $DRIVE
 }
 
 main $1
